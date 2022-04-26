4bec9750565178f8cd93fabe7ab1d8b6f4df7b7b
phanthuy010801 <phanthuthuy855@gmail.com>
2022-04-26 14:32:56 +0700

    kiet

--- LibraryManager/Properties/Resources.Designer.cs
+++ LibraryManager/Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace LibraryManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_borrow_book_64px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_borrow_book_64px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_cancel_48px {
             get {
                 object obj = ResourceManager.GetObject("icons8_cancel_48px", resourceCulture);
