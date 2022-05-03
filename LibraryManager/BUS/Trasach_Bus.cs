7e73be4aad77dcf3426984267587bcd45f4701ab
thuy010801 <phanthuthuy855@gmail.com>
2022-05-04 02:31:06 +0700

    insert some code Trasach_DAO,. but not run

--- LibraryManager/BUS/Trasach_Bus.cs
+++ LibraryManager/BUS/Trasach_Bus.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LibraryManager.DAO;
+using 
+using System.Data;
 namespace LibraryManager.BUS
 {
     class Trasach_Bus
     {
+        Trasach_DAO ts_Dao = new Trasach_DAO();
+        public DataTable Loadls()
+        {
+            return ts_Dao.load_trasach();
+        }
+       public DataTable loadtk_dgia(string a)
+        {
+            return ts_Dao.loadtk_madg(a);
+        }
+        public DataTable loadtk_masach(string a)
+        {
+            return ts_Dao.loadtk_masach(a);
+        }
+        public void update()
+        {
+
+        }
     }
 }
