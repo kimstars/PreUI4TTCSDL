ae0fc0b6bf180759a27f69f6be457e832197e9a4
kimstars <buikika9@gmail.com>
2022-06-03 07:17:00 +0700

    fix to run

--- LibraryManager/DAO/DataProvider.cs
+++ LibraryManager/DAO/DataProvider.cs
@@ -12,7 +12,7 @@ namespace LibraryManager.DAO
 {
     class DataProvider
     {
-        static string provider = @"Data Source=DESKTOP-ON09CDA\MSSQLSERVER02;Initial Catalog=THUVIENMTA;Integrated Security=True";
+        static string provider = @"Data Source=CHU-TUAN-KIET;Initial Catalog=THUVIENMTA;Integrated Security=True";
         protected SqlConnection connect = new SqlConnection(provider);
 
         public DataTable GetData(string sql)
